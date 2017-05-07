3c95456a25f65facc22a5488e8055273e27625f1
Dominic Gaiero <dpgaiero@gmail.com>
2017-05-06 23:44:15 -0700

    output Files

---
+++ output_stuff/output.py
@@ -0,0 +1,17 @@
+import web
+
+urls = (
+  '/', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class Index(object):
+    def GET(self):
+        greeting = "Hello World"
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
