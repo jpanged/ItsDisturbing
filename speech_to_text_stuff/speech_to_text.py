fec846c0d7ba4066d254478df9cc5fba4be7bee9
alexgarcia98 <haloalex68@gmail.com>
2017-05-06 22:18:02 -0700

    added bee movie script

--- speech_to_text_stuff/speech_to_text.py
+++ speech_to_text_stuff/speech_to_text.py
@@ -9,11 +9,14 @@ speech_to_text = SpeechToTextV1(
    x_watson_learning_opt_out=False
 )
 
-with open(join(dirname(__file__), 'speech.wav'), 'rb') as audio_file:
-   output = (speech_to_text.recognize(
-      audio_file, content_type='audio/wav', timestamps=True,
-      word_confidence=True))
+with open(join(dirname(__file__), 'beemovie.wav'), 'rb') as audio_file:
+   output = json.dumps((speech_to_text.recognize(
+      audio_file, content_type='audio/wav', timestamps=False,
+      word_confidence=True)), indent=2)
 
+'''
    transcript = output['results'][0]['alternatives'][0]['transcript']
-print(transcript)
+'''
+
+print(output)
 
