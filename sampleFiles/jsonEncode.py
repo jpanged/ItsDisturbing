7c189cf0e923283d09950e0cfdb383364c325d3f
Dominic Gaiero <dpgaiero@gmail.com>
2017-05-06 22:19:30 -0700

    add jsonEncode.py

--- sampleFiles/jsonEncode.py
+++ sampleFiles/jsonEncode.py
@@ -1,13 +1,55 @@
 import json
 
+NLP_data = {
+    'tone': 'Person',
+    'text': 'Wayne',
+    'relevance': 0.3
+}
+
+
+tone_analysis = {
+    'emotion': '',
+    'writing': '',
+    'social': ''
+
+}
+
+tone_analysis_emotion = {
+    'anger': '',
+    'disguist': '',
+    'fear': '',
+    'joy': '',
+    'saddness': ''
+}
+tone_analysis_writing = {
+    'analytical': '',
+    'confident': '',
+    'tennetative': ''
+}
+
+tone_analysis_social = {
+    'openness': '',
+    'Conscientiousness': '',
+    'extroversion': '',
+    'agreeableness': '',
+    'emotional range': ''
+}
+
+tone = {
+    'analysis' : tone_analysis,
+    'emotion': tone_analysis_emotion,
+    'writing' : tone_analysis_writing,
+    'social' : tone_analysis_social
+}
+
 data = {
-   'name' : 'ACME',
-   'shares' : 100,
-   'price' : 542.23
+    'NLP' : NLP_data,
+    'tone' : tone
+
 }
 
 json_str = json.dumps(data)
 
 # Writing JSON data
 with open('data.json', 'w') as f:
-     json.dump(data, f)
+    json.dump(data, f)
