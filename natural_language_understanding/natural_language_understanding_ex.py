1480d000204c2d7c6aea17f3e545729891112533
Josiah Pang <pang.josiah@gmail.com>
2017-05-06 23:07:20 -0700

    Made function that returns entities

--- natural_language_understanding/natural_language_understanding_ex.py
+++ natural_language_understanding/natural_language_understanding_ex.py
@@ -3,20 +3,34 @@ from watson_developer_cloud import NaturalLanguageUnderstandingV1
 import watson_developer_cloud.natural_language_understanding.features.v1 as \
     features
 
-
+# 20:5047c647-9695-46a3-85c5-6c3fa0944f1c
+'''
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2017-02-27',
     username='93f6afbe-f487-4f2a-847c-5972d0aaff35',
     password='3Bntz00f6MCI')
 
-'''response = natural_language_understanding.analyze(
-    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
-         'Superman fears not Banner, but Wayne.',
-    features=[features.Entities(), features.Keywords()])'''
-
 response = natural_language_understanding.analyze(
-    text='According to all known laws of aviation, there is no way that a bee should be able to fly.',
-         #'Superman fears not Banner, but Wayne.',
+    text= input_stuff,
     features=[features.Entities(), features.Keywords()])
 
-print(json.dumps(response, indent=2))
+response = natural_language_understanding.analyze(
+    text= 'Superman fears not Banner, but Wayne. I am friends with Bob. Dominic.',
+    features=[features.Entities(), features.Keywords()])
+
+print(response["entities"])
+#print(json.dumps(response, indent=2))'''
+
+def nlp(input_stuff):
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2017-02-27',
+        username='93f6afbe-f487-4f2a-847c-5972d0aaff35',
+        password='3Bntz00f6MCI')
+
+    response = natural_language_understanding.analyze(
+        text= input_stuff,
+        features=[features.Entities(), features.Keywords()])
+
+    return(response["entities"])
+
+print(nlp("I wanna go to the White House."))
